49ffd833acee3abd9ec7fc4aa50707179ea1c77d
RomanR <romanbr@meta.ua>
2018-10-03 21:13:44 +0300

    2
    Change-Id: Ic47674f55b52a1a37f44e82ccaae0a9ef37f5fff

--- lesson-1/src/ua/lviv/lgs/homework_1.java
+++ lesson-1/src/ua/lviv/lgs/homework_1.java
@@ -15,7 +15,7 @@ public static void main(String[] args) {
 	System.out.println(a);
 	System.out.println(b);
 	System.out.println(c);
-	System.out.println(d);
+	System.out.println(d); 
 	System.out.println(e);
 	System.out.println(f);
 	System.out.println(g);
@@ -61,3 +61,4 @@ public static void main(String[] args) {
 }
 }
 //first java project on github...
+//second
